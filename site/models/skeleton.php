3bb625c4ba29267f1a1b67ca447dc07b36d6d2e0
Luu Hoang Nam <hoangnam0705@icloud.com>
2014-08-13 09:32:49 +0700

    Add table features

--- site/models/skeleton.php
+++ site/models/skeleton.php
@@ -16,6 +16,21 @@ class SkeletonModelSkeleton extends JModelItem
     protected $msg;
 
     /**
+     * Returns a reference to the a Table object, always creating it.
+     *
+     * @param       string $type   The table type to instantiate
+     * @param       string $prefix A prefix for the table class name. Optional.
+     * @param       array  $config Configuration array for model. Optional.
+     *
+     * @return      JTable  A database object
+     * @since       2.5
+     */
+    public function getTable($type = 'Skeleton', $prefix = 'SkeletonTable', $config = [])
+    {
+        return JTable::getInstance($type, $prefix, $config);
+    }
+
+    /**
      * Get the message
      * @return string The message to be displayed to the user
      */
